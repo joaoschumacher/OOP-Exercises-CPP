78350c8b7e1c6391a3a827d91bdf5777f131edbd
João Pedro Schumacher <joaopedroschumacher@gmail.com>
2025-03-09 18:25:09 -0300

    Exercício 1

---
+++ lista_01/ex01/ex1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Retangulo.cpp"
+
+using namespace std;
+
+int main() {
+    cout << "Digite uma altura: ";
+    int altura;
+    cin >> altura;
+    cout << "Digite uma base: ";
+    int base;
+    cin >> base;
+
+    Retangulo retangulo = Retangulo(altura, base);
+    cout << "A area do retangulo e: " << retangulo.getArea();
+    return 0;
+}
